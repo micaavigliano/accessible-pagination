ad29fbfd45acc0548e103c95589dcb4d95c226ee
Micaela Avigliano <micaela.avigliano@gmail.com>
2024-10-29 12:49:33 -0300

    fix properties

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -51,28 +51,29 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, nextPa
         >
           <GrFormPrevious className=''/>
         </button>
-        {pageNumbers.map((number) => {
-          if (number === 'left-ellipsis' || number === 'right-ellipsis') {
+        <ol className='flex gap-3'>
+          {pageNumbers.map((number) => {
+            if (number === 'left-ellipsis' || number === 'right-ellipsis') {
+              return (
+                <span key={number} className='relative top-5' aria-label='ellipsis'>
+                  ...
+                </span>
+              );
+            }
             return (
-              <span key={number} className='relative top-5' aria-label='ellipsis'>
-                ...
-              </span>
+              <li aria-setsize={totalPages} aria-posinset={typeof number === 'number' ? number : undefined} key={`page-${number}`}>
+                <button
+                  onClick={() => goToPage(Number(number))}
+                  className={currentPage === Number(number) ? 'underline underline-offset-3 border-zinc-300' : ''}
+                  aria-label={`go to page ${number}`}
+                  aria-current={currentPage === Number(number) && 'page'}
+                >
+                  {number}
+                </button>
+              </li>
             );
-          }
-          return (
-            <button
-              key={`page-${number}`}
-              onClick={() => goToPage(Number(number))}
-              className={currentPage === Number(number) ? 'underline underline-offset-3 border-zinc-300' : ''}
-              aria-label={`go to page ${number}`}
-              aria-current={currentPage === Number(number) && 'page'}
-              aria-setsize={totalPages}
-              aria-posinset={totalPages}
-            >
-              {number}
-            </button>
-          );
-        })}
+          })}
+        </ol>
         <button
           onClick={nextPage}
           disabled={currentPage >= totalPages}
